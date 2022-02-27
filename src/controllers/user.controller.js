75a764de7ec59e6dfdab25924f128ba1c818488b
Tushar <tusharkumarsahu1998@gmail.com>
2022-02-27 15:49:17 +0530

    user controller

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+// router 
+const router = express.Router();
+
+// importing model
+const User = require("../models/user.model");
+
+// CRUD oprations-------------------------------------------------------->
+
+
+
+
+// exporting router
+module.exports = router;
